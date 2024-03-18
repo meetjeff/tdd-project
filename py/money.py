e403ef59dee202bea74a783420549c686d336a3a
jeff.hsu <jeff.hsu@nextfortune.tw>
2024-03-19 02:04:37 +0800

    test: verified behavior when modifying an existing exchange rate

--- py/money.py
+++ py/money.py
@@ -6,6 +6,9 @@ class Money:
     def __str__(self):
         return f"{self.currency} {self.amount:0.2f}"
     
+    def __repr__(self):
+        return self.__str__()
+    
     def __eq__(self, other):
         return self.amount == other.amount and self.currency == other.currency
 

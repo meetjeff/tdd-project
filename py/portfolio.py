11bd55158364e39ad56d04d30776b189d7561d70
jeff.hsu <jeff.hsu@nextfortune.tw>
2024-03-18 23:29:35 +0800

    feat: added Bank; refactored Portfolio to use Bank

--- py/portfolio.py
+++ py/portfolio.py
@@ -7,22 +7,15 @@ class Portfolio:
     def add(self, *moneys):
         self.moneys.extend(moneys)
 
-    def evaluate(self, currency):
+    def evaluate(self, bank, currency):
         total = 0.0
         failures = []
         for m in self.moneys:
             try:
-                total += self.__convert(m, currency)
-            except KeyError as ke:
-                failures.append(ke)
+                total += bank.convert(m, currency).amount
+            except Exception as ex:
+                failures.append(ex)
         if failures:
             failureMessage = ",".join(f.args[0] for f in failures)
             raise Exception("Missing exchange rate(s):[" + failureMessage + "]")
         return Money(total, currency)
-    
-    def __convert(self, aMoney, aCurrency):
-        exchangeRates = {'EUR->USD': 1.2, 'USD->KRW': 1100}
-        if aMoney.currency == aCurrency:
-            return aMoney.amount
-        key = aMoney.currency + '->' + aCurrency
-        return aMoney.amount * exchangeRates[key]
